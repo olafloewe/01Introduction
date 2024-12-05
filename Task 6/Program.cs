ad1f83ee9bda58a5d24a8d4625b595b84dd17c2d
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:34:45 +0100

    task 6 completed

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace Task_6 {
     internal class Program {
         static void Main(string[] args) {
+
+            float a;
+            float b;
+            float c;
+            float x;
+
+            while (true) {
+                // getting user input and guardian clauses for miss input
+                Console.WriteLine("Enter coefficients A, B, C and the value of X to calculate Y = ax^2 + bx + c");
+                Console.Write("A: ");
+                if (!float.TryParse(Console.ReadLine(), out a)) continue;
+                Console.Write("B: ");
+                if (!float.TryParse(Console.ReadLine(), out b)) continue;
+                Console.Write("C: ");
+                if (!float.TryParse(Console.ReadLine(), out c)) continue;
+                Console.Write("X: ");
+                if (!float.TryParse(Console.ReadLine(), out x)) continue;
+
+                Console.WriteLine($"The value of Y for the given numbers is: { (a*(x*x)) + (b*x) + c }");
+            }
         }
     }
 }
