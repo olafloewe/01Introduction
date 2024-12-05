49dd5d1a9a1be6d9ec9b0b7f678133f3f5a400b9
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:18:10 +0100

    task 3 completed

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace Task_3 {
     internal class Program {
         static void Main(string[] args) {
+
+
+                /*
+                Errors during compilation:
+
+                line 1 missing ;
+                line 2 no type for y
+                line 4 trying to redefine already used variable name and trying to define int as a string return
+
+                Errors during execution:
+
+                line 6 cant divide by 0
+                */
+
+
+                int x = 5;
+                int y = 0;
+
+                y = int.Parse(Console.ReadLine()); // to not divide by 0
+
+                Console.WriteLine(x / y); 
+
         }
     }
 }
