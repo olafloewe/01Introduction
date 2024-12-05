e72f14651fab9a29ed26837cf731082ed5801617
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:48:34 +0100

    task 12 complete

--- Task 12/Program.cs
+++ Task 12/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Task_12 {
     internal class Program {
         static void Main(string[] args) {
+            int hei = 3;
+            int wid = 5;
+
+            for (int i = 0; i < hei; i++) {
+                for (int j = 0; j < wid; j++) {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
