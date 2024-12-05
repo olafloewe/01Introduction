79c57e22317e7a68277d4f7601a8f12ca083cdbb
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:27:50 +0100

    task 5 completed

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace Task_5 {
     internal class Program {
         static void Main(string[] args) {
+
+            float conversion = 4.06F;
+            float pln;
+
+            while (true) {
+                // getting user input and guardian clauses for miss input
+                Console.WriteLine("Enter PLN to be converted to USD: ");
+                Console.Write("PLN: ");
+                if (!float.TryParse(Console.ReadLine(), out pln)) continue;
+
+                float usd = (float)Math.Round((pln / conversion), 2);
+
+                Console.WriteLine($"USD: {usd} from PLN: {pln}");
+            }
         }
     }
 }
