a5eccc6bb76e0fc42e22737336e1826b01b6e390
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:24:07 +0100

    task 4 completed

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Task_4 {
     internal class Program {
         static void Main(string[] args) {
+
+            float conversion = 4.06F;
+
+            float pln = 20;
+            float usd = (float)Math.Round((pln / conversion), 2);
+            
+            Console.WriteLine($"USD: {usd} from PLN: {pln}");
+            
         }
     }
 }
