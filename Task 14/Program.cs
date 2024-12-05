df9c5295cbaff6bce13f56c31237b30ed1177309
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 13:11:15 +0100

    task 14 completed

--- Task 14/Program.cs
+++ Task 14/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task_14 {
+namespace Task_13 {
     internal class Program {
         static void Main(string[] args) {
+
+            char one = ' ';
+            char two = 'O';
+            char three = ' ';
+            char four = 'O';
+            char five = ' ';
+            char six = ' ';
+            char seven = 'X';
+            char eight = 'X';
+            char nine = 'X';
+
+            // jagged char array for board
+            char[][] field = {
+                new char [] { ' ',one,' ','|',' ',two,' ','|',' ',three,' '},
+                new char [] { '-','-','-','+','-','-','-','+','-','-','-' },
+                new char [] {' ',four,' ','|',' ',five,' ','|',' ',six,' '},
+                new char [] {'-','-','-','+','-','-','-','+','-','-','-'},
+                new char [] {' ',seven,' ','|',' ',eight,' ','|',' ',nine,' '}
+            };
+
+            // print board
+            for (int i = 0; i < field.Length; i++) {
+                for (int j = 0; j < field[i].Length; j++) {
+                    Console.Write(field[i][j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
