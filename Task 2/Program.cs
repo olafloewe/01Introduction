71cc0a2ae78381c3ec6be1976e74ef8c2fe6e6ec
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 12:09:09 +0100

    task 2 completed

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Task_2 {
     internal class Program {
         static void Main(string[] args) {
+
+
+            float hei;
+            float wid;
+
+            while (true) {
+
+                // getting user input and guardian clauses for miss input
+                Console.WriteLine("Enter the height and width of a rectangle to calculate its area: ");
+                Console.Write("height: ");
+                if (!float.TryParse(Console.ReadLine(), out hei)) continue;
+                Console.Write("width: ");
+                if (!float.TryParse(Console.ReadLine(), out wid)) continue;
+
+                Console.WriteLine($"Rectangle area: {wid*hei}");
+            }
+
         }
     }
 }
